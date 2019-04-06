7b35a8049339ead724dc279b5b60655ee476a078
zaboevai <zaboevai@yandex.ru>
2019-04-06 14:33:53 +0300

    1 часть диплома, всю логику перенес в метод определения ближайшей цели, переписал логику так, чтобы код читался

--- zaboev.py
+++ zaboev.py
@@ -4,19 +4,13 @@ from astrobox.core import Dron
 class DragonDrone(Dron):
 
     def on_born(self):
-        self.target = self.asteroids[0]
         self.move_at(self.get_near_target())
 
     def on_stop_at_asteroid(self, asteroid):
         self.load_from(asteroid)
 
     def on_load_complete(self):
-        if self.free_space > 10:
-            self.move_at(self.get_near_target())
-        elif 0 < self.free_space <= 10 and self.distance_to(self.get_near_target()) < 10:
-            self.move_at(self.get_near_target())
-        else:
-            self.move_at(self.my_mathership)
+        self.move_at(self.get_near_target())
 
     def on_stop_at_mathership(self, mathership):
         self.unload_to(mathership)
@@ -25,13 +19,18 @@ class DragonDrone(Dron):
         self.move_at(self.get_near_target())
 
     def get_near_target(self):
-        dict_by_distance = {}
+        targets = {}
         for asteroid in self.asteroids:
-            dict_by_distance[asteroid] = self.distance_to(asteroid)
-        dict_by_distance = sorted(dict_by_distance.items(), key=lambda kv: kv[1])
+            targets[asteroid] = self.distance_to(asteroid)
+        targets = dict(sorted(targets.items(), key=lambda targets: targets[1]))
 
-        for asteroid in dict_by_distance:
-            if asteroid[0].payload > 0:
-                return asteroid[0]
+        for target, distance in targets.items():
+            if target.payload > 0:
+                if self.free_space > 10:
+                    return target
+                elif 0 < self.free_space <= 10 and distance < 10:
+                    return target
+                else:
+                    return self.my_mathership
         else:
             return self.my_mathership
