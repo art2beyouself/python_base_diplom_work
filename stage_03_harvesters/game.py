43b8207e6331e2a355bf214702db08109c21ec45
Alexander Nedovizin <parkit74@gmail.com>
2020-05-05 22:48:46 +0500

    Третий этап: дроны засыпают на базе

--- stage_03_harvesters/game.py
+++ stage_03_harvesters/game.py
@@ -5,7 +5,6 @@
 from astrobox.space_field import SpaceField
 from stage_03_harvesters.reaper import ReaperDrone
 from stage_03_harvesters.driller import DrillerDrone
-# TODO тут импортировать своих дронов
 from zaboev import ZaboevDrone
 
 NUMBER_OF_DRONES = 5
@@ -13,12 +12,11 @@ NUMBER_OF_DRONES = 5
 if __name__ == '__main__':
     scene = SpaceField(
         speed=5,
-        asteroids_count=20,
+        asteroids_count=15,
     )
-    # TODO создать их
     team_1 = [ZaboevDrone() for _ in range(NUMBER_OF_DRONES)]
-
-    # TODO и побороть противников!
     team_2 = [ReaperDrone() for _ in range(NUMBER_OF_DRONES)]
     scene.go()
 
+# TODO - Обновите астробокс до версии 1.4.0
+# TODO - Ваши дроны засыпают в середине игры, поэтому командда проигрывает
